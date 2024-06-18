4017ab803084809f71f81706e4eea60bf3a408ab
ngthecoder <ngoto0208@gmail.com>
2024-06-18 23:04:47 +0900

    prints command not found

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Print("$ ")
+
+	inputWithDelimiter, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	input := strings.Trim(inputWithDelimiter, "\n")
+
+	fmt.Fprintf(os.Stdout, "%s: command not found", input)
 }
