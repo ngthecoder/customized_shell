f2f3472ce66663e42f07407bd016ce0a1a91c3f1
ngthecoder <ngoto0208@gmail.com>
2024-06-18 23:20:45 +0900

    implemented exit cmd

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+type Command struct {
+	Name string
+	Args []string
+}
+
 func main() {
 	for {
 		fmt.Print("$ ")
@@ -17,8 +22,28 @@ func main() {
 			return
 		}
 
-		input := strings.Trim(inputWithDelimiter, "\n")
+		command := parseInput(strings.Trim(inputWithDelimiter, "\n"))
 
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+		switch command.Name {
+		case "exit":
+			if len(command.Args) == 1 && command.Args[0] == "0" {
+				return
+			} else if len(command.Args) != 1 {
+				fmt.Println("exit: wrong number of arguments")
+			} else {
+				fmt.Println("exit: invalid argument")
+			}
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", command.Name)
+		}
+	}
+}
+
+func parseInput(input string) Command {
+	parts := strings.Split(input, " ")
+
+	return Command{
+		Name: parts[0],
+		Args: parts[1:],
 	}
 }
